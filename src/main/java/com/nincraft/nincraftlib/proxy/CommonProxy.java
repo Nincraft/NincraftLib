3ff87d4524df1b5e67f9db2356f0205f27dd14e7
Nincodedo <nincodedo@gmail.com>
2015-11-24 22:18:15 -0600

    Actually register handlers

--- src/main/java/com/nincraft/nincraftlib/proxy/CommonProxy.java
+++ src/main/java/com/nincraft/nincraftlib/proxy/CommonProxy.java
@@ -1,13 +1,21 @@
 package com.nincraft.nincraftlib.proxy;
 
 import com.nincraft.nincraftlib.handler.ConfigurationHandler;
+import com.nincraft.nincraftlib.handler.DamageModifierHandler;
+import com.nincraft.nincraftlib.handler.ProcHandler;
+import com.nincraft.nincraftlib.handler.ThunderJoinHandler;
 
 import cpw.mods.fml.common.FMLCommonHandler;
+import net.minecraftforge.common.MinecraftForge;
 
 public abstract class CommonProxy implements IProxy {
 
 	public void registerEventHandlers() {
 		FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+		MinecraftForge.EVENT_BUS.register(new ThunderJoinHandler());
+		MinecraftForge.EVENT_BUS.register(new ProcHandler());
+		MinecraftForge.EVENT_BUS.register(new DamageModifierHandler());
+
 	}
 
 }
